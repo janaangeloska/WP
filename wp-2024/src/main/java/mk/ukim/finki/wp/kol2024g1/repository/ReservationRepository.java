0b0a7c32a44e3184d5cb8d408cb3a9681a9bf701
Jana Angeloska <132594803+janaangeloska@users.noreply.github.com>
2025-01-24 01:53:44 +0100

    Add files via upload

---
+++ wp-2024/src/main/java/mk/ukim/finki/wp/kol2024g1/repository/ReservationRepository.java
@@ -0,0 +1,6 @@
+package mk.ukim.finki.wp.kol2024g1.repository;
+
+import mk.ukim.finki.wp.kol2024g1.model.Reservation;
+
+public interface ReservationRepository extends JpaSpecificationRepository<Reservation,Long>{
+}
